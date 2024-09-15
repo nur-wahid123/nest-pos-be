7335cfa79c0ce708b73e57f50df3e70a860fb7e1
infanur <fajarnurwahid99@gmail.com>
2024-09-15 09:37:08 +0700

    feat: add new put request to update uom

--- src/modules/master/uoms/uoms.controller.ts
+++ src/modules/master/uoms/uoms.controller.ts
@@ -1,7 +1,8 @@
-import { Body, ClassSerializerInterceptor, Controller, Get, Post, UseInterceptors } from "@nestjs/common";
+import { Body, ClassSerializerInterceptor, Controller, Get, Post, Put, UseInterceptors } from "@nestjs/common";
 import { UomsService } from "./uoms.service";
 import { ResponseInterceptor } from "src/common/interceptors/response.interceptor";
 import { CreateUomDto } from "./dto/create-uom.dto";
+import { UpdateUomDto } from "./dto/update-uom.dto";
 
 @Controller('uoms')
 @UseInterceptors(new ResponseInterceptor(), ClassSerializerInterceptor)
@@ -19,5 +20,11 @@ export class UomsController {
     createUom(@Body() createUomDto: CreateUomDto) {
         // return "ha"
         return this.uomsService.create(createUomDto)
+    }
+
+    @Put()
+    updateUom(@Body() updateUomDto: UpdateUomDto) {
+        // return "ha"
+        return this.uomsService.update(updateUomDto)
     }
 }
